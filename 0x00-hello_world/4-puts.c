3f25191479e1861491f1bd51ef0138cc808dbe74
emai emmanuel <amalachukwuudokaemmanuel@gmail.com>
2023-02-20 08:49:23 -0800

    think i got it right

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main -Entry point
  *
- * Returns 0
+ * Returns :Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0) ;
 }
