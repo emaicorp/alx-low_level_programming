fb45788a27ac761e4b91fa68cd23240006edecde
emai emmanuel <amalachukwuudokaemmanuel@gmail.com>
2023-02-20 16:14:26 -0800

    yay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(1,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
