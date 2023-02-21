d9439a116d78be946ff9c5d89e0f92b72ae2a8c8
emai emmanuel <amalachukwuudokaemmanuel@gmail.com>
2023-02-20 16:10:02 -0800

    yay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
 int main(void)
 {
-	fwrite(STDOUT_FILENO,"and that piece of art is usefu\n" " - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
